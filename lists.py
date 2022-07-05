3913f68f5059e7ca3ed0a0d61ec0d8ec39ebd14a
dawood <dawood.khan@royalmail.com>
2022-07-05 10:26:20 +0100

    Add files ignored by mistake.

---
+++ lists.py
@@ -0,0 +1,25 @@
+my_list = ["Banana", "Cherry", "Apple", "Tomato"]
+print(f'my_list: {my_list}')
+
+my_list.append("Pear")
+print(f'Append Pear: {my_list}')
+
+my_list.pop(3)
+print(f'Pop 3: {my_list}')
+
+my_list.insert(3, "Tomato")
+print(f'Insert Tomato at index 3: {my_list}')
+
+my_list.remove("Cherry")
+print(f'Remove Cherry: {my_list}')
+
+my_list.reverse()
+print(f'Reverse list: {my_list}')
+
+my_list.sort()
+print(f'Sort list: {my_list}')
+
+my_list2 = [0] * 10
+print(f"New list 0 * 18: {my_list2}")
+
+print("print it again")
